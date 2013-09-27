72e3a95625a0a5b940597890be6e26eeebf205d7
liuji <liujiboy@163.com>
2013-09-27 20:18:47 +0800

    add 010

--- 009/Test3/Test3/main.cpp
+++ 009/Test3/Test3/main.cpp
@@ -7,6 +7,9 @@
 //
 
 #include <iostream>
+#include <cmath>
+#include <string>
+#include <stdexcept>
 using namespace std;
 class Point {
 private:
@@ -14,6 +17,7 @@ private:
     float y;
     
 public:
+    
     Point(float x=0,float y=0)
     {
         this->x=x;
@@ -24,24 +28,58 @@ public:
         this->x=p.x;
         this->y=p.y;
     }
-    
+    float distance(const Point&p)
+    {
+        return sqrt((this->x-p.x)*(this->x-p.x)+(this->y-p.y)*(this->y-p.y));
+    }
 };
 class Triangle {
 private:
     Point a;
     Point b;
     Point c;
-    
+    float ab;
+    float ac;
+    float bc;
 public:
     Triangle(const Point&p1,const Point&p2
              ,const Point&p3):a(p1),b(p2),c(p3)
     {
-       
+        this->ab=a.distance(b);
+        this->ac=a.distance(c);
+        this->bc=b.distance(c);
+        if(ab+ac>bc&&ab+bc>ac&&bc+ac>ab)
+        {
+            
+        }else{
+            throw runtime_error("不是三角形");
+            
+        }
+    }
+    float getLength()
+    {
+        
+        return ab+ac+bc;
+    }
+    float getArea()
+    {
+        float p=this->getLength()/2;
+        return sqrt(p*(p-ab)*(p-ac)*(p-bc));
     }
 };
 int main(int argc, const char * argv[])
 {
-    
-    return 0;
+    Point a(0,0);
+    Point b(0,0);
+    Point c(0,1);
+    try{
+        Triangle t(a, b, c);
+        cout<<t.getLength()<<endl;
+        cout<<t.getArea()<<endl;
+    }catch(runtime_error & message)
+    {
+        cout<<message.what()<<endl;
+    }
+        return 0;
 }
 
